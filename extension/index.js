68657285ae7ebfefeb76c063cd5309f015304b96
zoton2 <zoton2@gmail.com>
2019-04-28 23:55:05 +0100

    Change extension to TypeScript/change deps

--- extension/index.js
+++ extension/index.js
@@ -1,5 +1,13 @@
-'use strict';
-
-module.exports = function(nodecg) {
-	
-}
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+var nodecgApiContext = __importStar(require("./util/nodecg-api-context"));
+module.exports = function (nodecg) {
+    nodecgApiContext.set(nodecg);
+};
+//# sourceMappingURL=index.js.map
---
+++ src/extension/index.ts
@@ -0,0 +1,6 @@
+import { NodeCG } from '../../../../types/server';
+import * as nodecgApiContext from './util/nodecg-api-context';
+
+export = (nodecg: NodeCG) => {
+  nodecgApiContext.set(nodecg);
+}

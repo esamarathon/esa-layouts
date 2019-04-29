66b714d7978a622d43bb50e47910d30e4f08e08a
zoton2 <zoton2@gmail.com>
2019-04-29 22:11:27 +0100

    Add "Toggle Timer" Stream Deck button

--- extension/streamdeck.js
+++ extension/streamdeck.js
@@ -13,12 +13,69 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var nodecgApiContext = __importStar(require("./util/nodecg-api-context"));
 var streamdeck_util_1 = __importDefault(require("streamdeck-util"));
 var nodecg = nodecgApiContext.get();
+var initDone = false;
 var sd = new streamdeck_util_1.default({
     key: nodecg.bundleConfig.streamdeck.key,
     port: nodecg.bundleConfig.streamdeck.port,
     debug: nodecg.bundleConfig.streamdeck.debug,
 });
 sd.on('init', function () {
-    // connection initilized
+    if (!initDone) {
+        init();
+    }
+    initDone = true;
+    // com.esamarathon.streamdeck.timer
+    // Set default text on buttons.
+    var timerButtons = sd.findButtonsWithAction('com.esamarathon.streamdeck.timer');
+    timerButtons.forEach(function (button) {
+        sd.updateButtonText(button.context, 'Start\nTimer');
+    });
 });
+function init() {
+    // com.esamarathon.streamdeck.timer
+    // Controls the text on the buttons.
+    var timer = nodecg.Replicant('timer', 'nodecg-speedcontrol');
+    timer.on('change', function (newVal, oldVal) {
+        var buttons = sd.findButtonsWithAction('com.esamarathon.streamdeck.timer');
+        buttons.forEach(function (button) {
+            switch (newVal.state) {
+                case 'stopped':
+                    sd.updateButtonText(button.context, 'Start\nTimer');
+                    break;
+                case 'running':
+                    sd.updateButtonText(button.context, 'Stop\nTimer');
+                    break;
+                case 'paused':
+                    sd.updateButtonText(button.context, 'Resume\nTimer');
+                    break;
+                case 'finished':
+                    sd.updateButtonText(button.context, 'Reset\nTimer');
+                    break;
+            }
+        });
+    });
+    sd.on('keyUp', function (data) {
+        // com.esamarathon.streamdeck.timer
+        // Controls the nodecg-speedcontrol timer when the button is pressed.
+        // USES "UNSUPPORTED" API STUFF, NEEDS CHANGING IN FUTURE.
+        // ALSO nodecg.sendMessageToBundle NEEDS ADDING TO THEIR TYPINGS!
+        if (data.action === 'com.esamarathon.streamdeck.timer') {
+            switch (timer.value.state) {
+                case 'stopped':
+                case 'paused':
+                    // @ts-ignore
+                    nodecg.sendMessageToBundle('startTime', 'nodecg-speedcontrol');
+                    break;
+                case 'running':
+                    // @ts-ignore
+                    nodecg.sendMessageToBundle('finishTime', 'nodecg-speedcontrol');
+                    break;
+                case 'finished':
+                    // @ts-ignore
+                    nodecg.sendMessageToBundle('resetTime', 'nodecg-speedcontrol');
+                    break;
+            }
+        }
+    });
+}
 //# sourceMappingURL=streamdeck.js.map
--- src/extension/streamdeck.ts
+++ src/extension/streamdeck.ts
@@ -2,7 +2,7 @@ import * as nodecgApiContext from './util/nodecg-api-context';
 import streamdeckUtil from 'streamdeck-util';
 
 const nodecg = nodecgApiContext.get();
-
+let initDone = false;
 const sd = new streamdeckUtil({
   key: nodecg.bundleConfig.streamdeck.key,
   port: nodecg.bundleConfig.streamdeck.port,
@@ -10,5 +10,62 @@ const sd = new streamdeckUtil({
 });
 
 sd.on('init', () => {
-  // connection initilized
+  if (!initDone) { init(); }
+  initDone = true;
+
+  // com.esamarathon.streamdeck.timer
+  // Set default text on buttons.
+  const timerButtons = sd.findButtonsWithAction('com.esamarathon.streamdeck.timer');
+  timerButtons.forEach((button) => {
+    sd.updateButtonText(button.context, 'Start\nTimer');
+  });
 });
+
+function init() {
+  // com.esamarathon.streamdeck.timer
+  // Controls the text on the buttons.
+  const timer = nodecg.Replicant<any>('timer', 'nodecg-speedcontrol');
+  timer.on('change', (newVal: any, oldVal: any) => {
+    const buttons = sd.findButtonsWithAction('com.esamarathon.streamdeck.timer');
+    buttons.forEach((button) => {
+      switch (newVal.state) {
+        case 'stopped':
+          sd.updateButtonText(button.context, 'Start\nTimer');
+          break;
+        case 'running':
+          sd.updateButtonText(button.context, 'Stop\nTimer');
+          break;
+        case 'paused':
+          sd.updateButtonText(button.context, 'Resume\nTimer');
+          break;
+        case 'finished':
+          sd.updateButtonText(button.context, 'Reset\nTimer');
+          break;
+      }
+    });
+  });
+
+  sd.on('keyUp', (data: any) => {
+    // com.esamarathon.streamdeck.timer
+    // Controls the nodecg-speedcontrol timer when the button is pressed.
+    // USES "UNSUPPORTED" API STUFF, NEEDS CHANGING IN FUTURE.
+    // ALSO nodecg.sendMessageToBundle NEEDS ADDING TO THEIR TYPINGS!
+    if (data.action === 'com.esamarathon.streamdeck.timer') {
+      switch (timer.value.state) {
+        case 'stopped':
+        case 'paused':
+          // @ts-ignore
+          nodecg.sendMessageToBundle('startTime', 'nodecg-speedcontrol');
+          break;
+        case 'running':
+          // @ts-ignore
+          nodecg.sendMessageToBundle('finishTime', 'nodecg-speedcontrol');
+          break;
+        case 'finished':
+          // @ts-ignore
+          nodecg.sendMessageToBundle('resetTime', 'nodecg-speedcontrol');
+          break;
+      }
+    }
+  });
+}

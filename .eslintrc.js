78a5313e84835b614e65dbd28036ecab311000d3
zoton2 <zoton2@gmail.com>
2019-04-29 01:07:33 +0100

    Added eslint (to the best of my ability)

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: 'airbnb-base',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+    'import/no-extraneous-dependencies': [error, { devDependencies: true }]
+  },
+};

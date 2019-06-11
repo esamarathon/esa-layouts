c181e97da66a5ed424bd5bffd023e003318ba79f
zoton2 <zoton2@gmail.com>
2019-06-11 15:52:55 +0100

    Add unpausing/resetting to BigButton functionality

--- src/extension/timer.ts
+++ src/extension/timer.ts
@@ -16,16 +16,23 @@ mq.on('BigButton', (data: any) => {
   const buttonID = data.button_id - 1;
 
   // Make sure we're listening for the right message.
+  // The message should *never* be used for anything else, but I like to be safe.
   if (!data.time && !data.button_message_count) {
     return;
   }
 
+  // Note: the nodecg-speedcontrol bundle will check if it *can* do these actions,
+  // we do not need to check that here.
   switch (sc.timer.value.state) {
     case 'stopped':
+    case 'paused':
       sc.startTimer();
       break;
     case 'running':
       sc.stopTimer(buttonID);
+      break;
+    case 'finished':
+      sc.resetTimer();
       break;
   }
 });

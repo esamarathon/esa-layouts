54c245703f8dcbf7f6cfc7a76cb57287bcb1cb14
zoton2 <zoton2@gmail.com>
2019-06-13 00:38:26 +0100

    Omnibar template

---
+++ src/graphics/omnibar/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import App from './main.vue';
+
+// eslint-disable-next-line no-new
+new Vue({
+  el: '#app',
+  render: h => h(App),
+});
---
+++ src/graphics/omnibar/main.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      msg: 'This will be the omnibar soom™.',
+    };
+  },
+};
+</script>
+
+<style lang="css">
+  body {
+    font-family: 'Barlow Condensed', sans-serif;
+    color: white;
+    overflow: hidden;
+    margin: 0;
+    padding: 0;
+  }
+
+  #app {
+    position: fixed;
+    width: 1920px;
+    height: 80px;
+    background-color: #56b983;
+    text-align: center;
+  }
+</style>

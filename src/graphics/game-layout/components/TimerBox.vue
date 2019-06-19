e3c428fb4ce60b07c1134a66c1221c9b46c73e38
zoton2 <zoton2@gmail.com>
2019-06-19 01:26:41 +0100

    Add colour to timer/add backup timer code

--- src/graphics/game-layout/components/TimerBox.vue
+++ src/graphics/game-layout/components/TimerBox.vue
@@ -2,6 +2,7 @@
   <div
     id="TimerBox"
     class="GameInfoBox FlexContainer"
+    :style="textColor"
   >
     {{ time }}
   </div>
@@ -15,17 +16,62 @@ export default {
   data() {
     return {
       time: '',
+      timer: Vue.prototype.$sc.timer,
+      backupTimerTO: setTimeout(0),
+      textColor: {
+        color: 'white',
+      },
     };
   },
   mounted() {
-    Vue.prototype.$sc.timer.on('change', this.updateData);
+    this.timer.on('change', this.updateData);
   },
   destroyed() {
-    Vue.prototype.$sc.timer.removeListener('change', this.updateData);
+    this.timer.removeListener('change', this.updateData);
   },
   methods: {
     updateData(timer) {
       this.time = timer.time;
+
+      // These colours need changing!
+      switch (timer.state) {
+      default:
+      case 'running':
+        this.textColor.color = 'white';
+        break;
+      case 'paused':
+      case 'stopped':
+        this.textColor.color = 'yellow';
+        break;
+      case 'finished':
+        this.textColor.color = 'orange';
+      }
+
+      // Backup timer (see below).
+      clearTimeout(this.backupTimerTO);
+      this.backupTimerTO = setTimeout(this.backupTimer, 1000);
+    },
+    // Backup timer that takes over if the connection to the server is lost.
+    // Based on the last timestamp that was received.
+    // When the connection is restored, the server timer will recover and take over again.
+    backupTimer() {
+      this.backupTimerTO = setTimeout(this.backupTimer, 200);
+      if (this.timer.value.state === 'running') {
+        const missedTime = Date.now() - this.timer.value.timestamp;
+        const timeOffset = this.timer.value.milliseconds + missedTime;
+        this.time = this.msToDuration(timeOffset);
+      }
+    },
+    msToDuration(ms) {
+      const seconds = Math.floor((ms / 1000) % 60);
+      const minutes = Math.floor((ms / (1000 * 60)) % 60);
+      const hours = Math.floor(ms / (1000 * 60 * 60));
+
+      const hoursStr = (hours < 10) ? `0${hours}` : `${hours}`;
+      const minutesStr = (minutes < 10) ? `0${minutes}` : `${minutes}`;
+      const secondsStr = (seconds < 10) ? `0${seconds}` : `${seconds}`;
+
+      return `${hoursStr}:${minutesStr}:${secondsStr}`;
     },
   },
 };
@@ -42,5 +88,6 @@ export default {
     line-height: 90%;
     padding: 15px;
     padding-top: 8px; /* Numbers are strange and this makes it look better. */
+    transition: 1s;
   }
 </style>

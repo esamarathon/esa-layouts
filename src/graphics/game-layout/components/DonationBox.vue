c5cb9e3d99d357182c1bec38c4bfcc16a7a5b422
zoton2 <zoton2@gmail.com>
2019-06-19 22:46:17 +0100

    Start of adding donations in the donations bar

---
+++ src/graphics/game-layout/components/DonationBox.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+    id="DonationBox"
+    class="FlexContainer"
+    :style="css"
+  >
+    {{ name }} - ${{ amount }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DonationBox',
+  props: {
+    name: {
+      type: String,
+      default: '',
+    },
+    amount: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      css: {
+        'background-color': 'purple',
+      },
+    };
+  },
+  created() {
+    // Choose a random colour when created (colours need editing).
+    const colours = ['red', 'blue', 'orange', 'purple'];
+    const rand = colours[Math.floor(Math.random() * colours.length)];
+    this.css['background-color'] = rand;
+  },
+};
+</script>
+
+<style scoped>
+  @import url('./FlexContainer.css');
+
+  #DonationBox {
+    height: 100%;
+    padding: 0 15px;
+    font-size: 30px;
+    border-left: 3px solid white;
+  }
+</style>
--- src/graphics/game-layout/components/DonationsBar.vue
+++ src/graphics/game-layout/components/DonationsBar.vue
@@ -1,16 +1,66 @@
 <template>
-  <div id="DonationsBar" />
+  <div
+    id="DonationsBar"
+    class="FlexContainer"
+  >
+    <div class="Header FlexContainer">
+      Latest<br>Donations
+    </div>
+    <div
+      id="DonationsContainer"
+      class="FlexContainer"
+    />
+  </div>
 </template>
 
 <script>
+import Vue from 'vue';
+import DonationBox from './DonationBox.vue';
+
+const DonationBoxClass = Vue.extend(DonationBox);
+
 export default {
   name: 'DonationsBar',
+  mounted() {
+    // fake donations coming in for testing
+    // setInterval(() => { this.addDonation('zoton2', 5); }, 2000);
+  },
+  methods: {
+    addDonation(name, amount) {
+      const instance = new DonationBoxClass({
+        propsData: {
+          name,
+          amount,
+        },
+      }).$mount();
+      document.getElementById('DonationsContainer').prepend(instance.$el);
+    },
+  },
 };
 </script>
 
 <style scoped>
+  @import url('./FlexContainer.css');
+
   #DonationsBar {
     position: fixed;
-    background-color: yellow;
+    justify-content: flex-start;
+  }
+
+  #DonationsBar > .Header {
+    font-size: 24px;
+    background-color: indigo;
+    height: 100%;
+    text-align: center;
+    line-height: 100%;
+    padding: 0 15px;
+    border-left: 3px solid white;
+  }
+
+  #DonationsBar > #DonationsContainer {
+    height: 100%;
+    white-space: nowrap;
+    overflow: hidden;
+    justify-content: flex-start;
   }
 </style>

0f9da23f263e1e03520f710e295c175410e9d1aa
zoton2 <zoton2@gmail.com>
2019-06-28 01:15:47 +0100

    Calculate capture area size without borders

--- src/graphics/game-layout/components/Capture.vue
+++ src/graphics/game-layout/components/Capture.vue
@@ -13,12 +13,23 @@ const captureChange = Vue.directive('captureChange', {
   inserted(el) {
     const cssID = el.id;
     const cssClass = el.classList[1]; // It *should* always be the second one.
-    const sizes = el.getBoundingClientRect();
+    const sizes = el.getClientRects();
+
+    // Get the widths of all the borders to figure out the position/size without them.
+    const topBorder = getComputedStyle(el).getPropertyValue('border-top-width');
+    const rightBorder = getComputedStyle(el).getPropertyValue('border-right-width');
+    const bottomBorder = getComputedStyle(el).getPropertyValue('border-bottom-width');
+    const leftBorder = getComputedStyle(el).getPropertyValue('border-left-width');
 
     nodecg.sendMessage('captureChange', {
       cssID,
       cssClass,
-      sizes,
+      sizes: {
+        x: sizes[0].x + Number(leftBorder),
+        y: sizes[0].y + Number(topBorder),
+        width: sizes[0].width - Number(rightBorder) - Number(leftBorder),
+        height: sizes[0].height - Number(bottomBorder) - Number(topBorder),
+      },
     });
   },
   unbind(el) {

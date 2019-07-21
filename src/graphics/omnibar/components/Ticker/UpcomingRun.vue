3092bebc20ff0bdbd2345b2ed8f213a7d5019612
zoton2 <zoton2@gmail.com>
2019-07-21 16:05:57 +0200

    Skip upcoming run in omnibar if should've already happened

--- src/graphics/omnibar/components/Ticker/UpcomingRun.vue
+++ src/graphics/omnibar/components/Ticker/UpcomingRun.vue
@@ -62,6 +62,12 @@ export default {
 
     const randNum = Math.floor(Math.random() * nextRunsCache.length);
     this.run = clone(nextRunsCache[randNum]);
+    if (this.run.scheduledS < (Date.now() / 1000)) {
+      nextRunsCache.splice(randNum, 1);
+      clearTimeout(fallback);
+      this.$emit('end');
+      return;
+    }
     this.when = moment.unix(this.run.scheduledS).fromNow();
     nextRunsCache.splice(randNum, 1);
     clearTimeout(fallback);

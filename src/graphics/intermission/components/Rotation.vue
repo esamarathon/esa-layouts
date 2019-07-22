8d785160dd5af04feb98ef523ecba58e4642db07
zoton2 <zoton2@gmail.com>
2019-07-22 03:53:55 +0200

    Don't rely on library to find next runs
    this doesn't fix my issue but *shrug*

--- src/graphics/intermission/components/Rotation.vue
+++ src/graphics/intermission/components/Rotation.vue
@@ -26,12 +26,39 @@ export default {
     };
   },
   created() {
-    NodeCG.waitForReplicants(sc.runDataActiveRun).then(() => this.refreshUpcomingRuns());
+    NodeCG.waitForReplicants(sc.runDataActiveRun, sc.runDataArray).then(() => this.refreshUpcomingRuns());
     nodecg.listenFor('forceRefreshIntermission', this.refreshUpcomingRuns);
   },
   methods: {
     refreshUpcomingRuns() {
-      this.nextRuns = sc.getNextRuns().slice(1, 4);
+      this.nextRuns = JSON.parse(JSON.stringify(this.getNextRuns()));
+      console.log(this.nextRuns);
+    },
+    getNextRuns() {
+      const nextRuns = [];
+      const indexOfCurrentRun = this.findIndexInRunDataArray(sc.runDataActiveRun.value);
+      for (let i = 2; i <= 4; i += 1) {
+        if (!sc.runDataArray.value[indexOfCurrentRun + i]) {
+          break;
+        }
+        nextRuns.push(JSON.parse(JSON.stringify(sc.runDataArray.value[indexOfCurrentRun + i])));
+      }
+      return nextRuns;
+    },
+    findIndexInRunDataArray(run) {
+      let indexOfRun = -1;
+
+      // Completely skips this if the run variable isn't defined.
+      if (run) {
+        for (let i = 0; i < sc.runDataArray.value.length; i += 1) {
+          if (run.id === sc.runDataArray.value[i].id) {
+            indexOfRun = i;
+            break;
+          }
+        }
+      }
+
+      return indexOfRun;
     },
   },
 };

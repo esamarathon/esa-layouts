9122d446790e6015385871d62d686449bc0a1b5f
zoton2 <zoton2@gmail.com>
2019-11-29 15:11:52 +0000

    Update method to get FFZ featured channels

--- src/extension/twitch-ext.ts
+++ src/extension/twitch-ext.ts
@@ -1,12 +1,18 @@
 import needle from 'needle';
+import { TwitchAPIData } from '../../../nodecg-speedcontrol/schemas';
 import * as nodecgApiContext from './util/nodecg-api-context';
 import { bundleConfig } from './util/nodecg-bundleconfig';
 
 const nodecg = nodecgApiContext.get();
+const apiData = nodecg.Replicant<TwitchAPIData>('twitchAPIData', 'nodecg-speedcontrol');
 
 if (bundleConfig.twitchExt.enable && bundleConfig.twitchExt.token) {
   // bad way of doing this, we need to change in the future
-  nodecg.listenFor('updateFeaturedChannels', 'nodecg-speedcontrol', setButtons);
+  apiData.on('change', (newVal, oldVal) => {
+    if (oldVal && newVal.featuredChannels.toString() !== oldVal.featuredChannels.toString()) {
+      setButtons(newVal.featuredChannels);
+    }
+  });
 }
 
 async function setButtons(usernames: string[]) {

b2e8c6bd2b0b16f63f9b0b86536f9ab883870377
zoton2 <zoton2@gmail.com>
2020-02-12 04:26:07 +0000

    Add try/catch while attempting to play commercials

--- src/extension/commercials.ts
+++ src/extension/commercials.ts
@@ -11,7 +11,7 @@ let commercialTO: NodeJS.Timeout;
  * Will attempt to play a commercial if >19 minutes is left for the run
  * and the estimate is higher than 39 minutes.
  */
-function playCommercial(): void {
+async function playCommercial(): Promise<void> {
   const run = sc.getCurrentRun();
   if (!run) {
     return;
@@ -19,9 +19,14 @@ function playCommercial(): void {
   const timeLeft = run && run.estimateS
     ? (run.estimateS + 60) - (sc.timer.value.milliseconds / 1000) : 0;
   if (run.estimateS && run.estimateS > (60 * (40 - 1)) && timeLeft > (60 * 20)) {
-    sc.sendMessage('twitchStartCommercial', { duration: 60 });
-    commercialTO = setTimeout(playCommercial, 1000 * 60 * 20);
-    nodecg().log.info('[Commercial] Triggered, will check again in 20 minutes');
+    try {
+      await sc.sendMessage('twitchStartCommercial', { duration: 60 });
+      commercialTO = setTimeout(playCommercial, 1000 * 60 * 20);
+      nodecg().log.info('[Commercial] Triggered, will check again in 20 minutes');
+    } catch (err) {
+      nodecg().log.warn('[Commercial] Could not successfully be triggered');
+      nodecg().log.debug('[Commercial] Could not successfully be triggered:', err);
+    }
   } else {
     nodecg().log.info('[Commercial] Does not need to be triggered,'
       + ' will not check again for this run');
@@ -43,9 +48,15 @@ sc.on('timerReset', () => {
 });
 
 // Trigger a Twitch commercial when on the relevant scene.
-obs.on('SwitchScenes', (data) => {
+obs.on('SwitchScenes', async (data) => {
   if (data['scene-name'] === config.obs.names.scenes.commercials) {
-    sc.sendMessage('twitchStartCommercial', { duration: 180 });
+    try {
+      await sc.sendMessage('twitchStartCommercial', { duration: 180 });
+      nodecg().log.info('[Commercial] Triggered on change to relevant scene');
+    } catch (err) {
+      nodecg().log.warn('[Commercial] Could not successfully be triggered');
+      nodecg().log.debug('[Commercial] Could not successfully be triggered:', err);
+    }
   }
 });
 

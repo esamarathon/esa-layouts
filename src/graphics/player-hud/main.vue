184e9b616d1907d4c2037b685bc8d643083ff86b
zoton2 <zoton2@gmail.com>
2020-02-12 01:14:23 +0000

    Simple Player HUD page to show if donations pending

---
+++ src/graphics/player-hud/main.vue
@@ -0,0 +1,50 @@
+<template>
+  <div :class="`PlayerHUD ${donationsToRead.length ? 'ToRead' : ''}`">
+    <template v-if="!donationsToRead.length">
+      Donations Pending
+    </template>
+    <template v-else>
+      No Donations Currently
+    </template>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import { State } from 'vuex-class';
+import { Donation } from 'types';
+
+@Component
+export default class extends Vue {
+  @State donationsToRead!: Donation;
+}
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Barlow+Condensed:400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap&subset=latin-ext,vietnamese');
+
+  body {
+    font-family: 'Barlow Condensed';
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+  }
+
+  .PlayerHUD {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    color: white;
+    background-color: black;
+    transition: background-color 1s;
+    text-shadow: 4px 4px 1px black;
+    text-align: center;
+    font-size: 18vh;
+  }
+
+  .ToRead {
+    background-color: rgb(0, 177, 15);
+  }
+</style>
---
+++ src/graphics/player-hud/store.ts
@@ -0,0 +1,36 @@
+import clone from 'clone';
+import { ReplicantBrowser } from 'nodecg/types/browser';
+import { DonationsToRead } from 'schemas';
+import Vue from 'vue';
+import Vuex, { Store } from 'vuex';
+
+Vue.use(Vuex);
+
+// Replicants and their types
+const reps: {
+  donationsToRead: ReplicantBrowser<DonationsToRead>;
+  [k: string]: ReplicantBrowser<unknown>;
+} = {
+  donationsToRead: nodecg.Replicant('donationsToRead'),
+};
+
+const store = new Vuex.Store({
+  state: {},
+  mutations: {
+    setState(state, { name, val }): void {
+      Vue.set(state, name, val);
+    },
+  },
+});
+
+Object.keys(reps).forEach((key) => {
+  reps[key].on('change', (val) => {
+    store.commit('setState', { name: key, val: clone(val) });
+  });
+});
+
+export default async function (): Promise<Store<{}>> {
+  return NodeCG.waitForReplicants(
+    ...Object.keys(reps).map((key) => reps[key]),
+  ).then(() => store);
+}

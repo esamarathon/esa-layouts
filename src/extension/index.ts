13f070c04dfa67b12a30f84690fe9ff8f93fa4bd
zoton2 <zoton2@gmail.com>
2020-03-23 14:10:31 +0000

    Check that config for event number is valid before mounting

--- src/extension/index.ts
+++ src/extension/index.ts
@@ -1,11 +1,21 @@
 /* eslint-disable global-require */
 
+import { Configschema } from 'configschema';
 import { NodeCG } from 'nodecg/types/server';
 import { set } from './util/nodecg';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export = (nodecg: NodeCG): { obs: any } => {
   set(nodecg);
+
+  // If `thisEvent` is 2, checks if we actually have 2 event shorts to
+  // pick from before mounting the extension.
+  const config: Configschema = nodecg.bundleConfig;
+  if (config.event.thisEvent === 2
+  && (typeof config.event.shorts === 'string' || config.event.shorts.length === 1)) {
+    throw new Error('event.thisEvent in config is set to 2 but you only '
+      + 'have 1 event short at event.shorts');
+  }
 
   require('./smb-relay');
   require('./obs-data');

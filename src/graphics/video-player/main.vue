9cffe6482c5d7ea57b9201bfbc4a5d8259c3639a
zoton2 <zoton2@gmail.com>
2020-05-01 16:38:54 +0100

    Allow video-player to work at any width/height correctly

--- src/graphics/video-player/main.vue
+++ src/graphics/video-player/main.vue
@@ -5,8 +5,8 @@
       ref="VideoPlayer"
       :style="{
         display: 'block',
-        width: '1920px',
-        height: '1080px'
+        width: '100%',
+        height: '100vh',
       }"
     >
       <source

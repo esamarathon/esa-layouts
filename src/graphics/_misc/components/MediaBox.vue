297f87d688d30ec8303e4852fcfcd3ce0ae0add4
zoton2 <zoton2@gmail.com>
2020-05-11 13:23:01 +0100

    Add code to media box to show current sponsor logo

--- src/graphics/_misc/components/MediaBox.vue
+++ src/graphics/_misc/components/MediaBox.vue
@@ -1,12 +1,57 @@
 <template>
-  <div class="Fixed Flex">
-    Media Box Content
+  <div
+    class="Fixed Flex"
+    :style="{
+      position: 'relative',
+    }"
+  >
+    <transition name="fade">
+      <img
+        v-if="current"
+        :key="current.id"
+        :src="current.url"
+        :style="{
+          position: 'absolute',
+          'object-fit': 'contain',
+          width: '100%',
+          height: '100%',
+          padding: '30px',
+          'box-sizing': 'border-box',
+        }"
+      >
+    </transition>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
+import { State } from 'vuex-class';
+import { SponsorLogos } from 'schemas';
+import { Asset } from 'types';
 
 @Component
-export default class extends Vue {}
+export default class extends Vue {
+  @State sponsorLogoAssets!: Asset[];
+  @State sponsorLogos!: SponsorLogos;
+
+  get current(): { id: string; url: string} | undefined {
+    if (!this.sponsorLogoAssets) {
+      return undefined;
+    }
+    const asset = this.sponsorLogoAssets.find((s) => s.sum === this.sponsorLogos.current?.sum);
+    return asset && this.sponsorLogos.current ? {
+      id: this.sponsorLogos.current.id,
+      url: asset.url,
+    } : undefined;
+  }
+}
 </script>
+
+<style scoped>
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity 1s;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+</style>

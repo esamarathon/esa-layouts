c22f7f371e18ecd3beb9d7b010ea95b15ac322ce
zoton2 <zoton2@gmail.com>
2020-05-11 12:59:58 +0100

    Always use default theme

--- src/graphics/_misc/theme.ts
+++ src/graphics/_misc/theme.ts
@@ -5,8 +5,6 @@ import { Configschema } from 'configschema';
 const { theme } = (nodecg.bundleConfig as Configschema).event;
 const defaultTheme = require('./themes/default.theme.css');
 
-if (!theme) {
-  defaultTheme.use();
-} else {
-  defaultTheme.use();
-}
+defaultTheme.use();
+
+// More themes will be included here when added.

f34f548722ef3060aab91e3d4f666d5206eef13a
zoton2 <zoton2@gmail.com>
2020-06-12 02:06:30 +0100

    Change RabbitMQ dashboard implementation

--- src/dashboard/rabbitmq-test/main.ts
+++ src/dashboard/rabbitmq-test/main.ts
@@ -1,6 +1,19 @@
+/* eslint no-new: off, @typescript-eslint/explicit-function-return-type: off */
+
 import type { Configschema } from 'configschema';
-import Component from 'esa-layouts-shared/rabbitmq/dashboard';
+import App from 'esa-layouts-shared/rabbitmq/dashboard';
+import Vue from 'vue';
+import vuetify from '../_misc/vuetify';
 
 const config = (nodecg.bundleConfig as Configschema);
 
-Component(config.rabbitmq.enable, config.useTestData);
+new Vue({
+  vuetify,
+  el: '#App',
+  render: (h) => h(App, {
+    props: {
+      enable: config.rabbitmq.enable,
+      useTestData: config.useTestData,
+    },
+  }),
+});

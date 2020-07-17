b17bcf6d5ddbef64d204431342488c95a1ed0936
zoton2 <zoton2@gmail.com>
2020-07-17 13:18:53 +0100

    Move back in code for restream changing from nodecg-speedcontrol

--- src/extension/restream.ts
+++ src/extension/restream.ts
@@ -1,6 +1,20 @@
 import type { Configschema } from 'configschema';
 import Restream from 'esa-layouts-shared/restream/extension';
+import SpeedcontrolUtil from 'speedcontrol-util';
 import { get as nodecg } from './util/nodecg';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const sc = new SpeedcontrolUtil(nodecg());
 const restream = new Restream(nodecg(), true, (nodecg().bundleConfig as Configschema).restream);
+
+// Change streams when run changes but not on server (re)start.
+let init = false;
+sc.runDataActiveRun.on('change', async (newVal, oldVal) => {
+  if (init && newVal?.id !== oldVal?.id) {
+    nodecg().log.info('[Restream] Attempting to update channels from run change');
+    const channels = newVal?.teams.map((t) => (
+      t.players[0] && t.players[0].social.twitch ? t.players[0].social.twitch : undefined
+    )) || [];
+    restream.updateMultipleInstances(channels);
+  }
+  init = true;
+});

010d171475a0f7430e2603cfbb8548bb99d67913
zoton2 <zoton2@gmail.com>
2020-07-23 21:19:08 +0100

    Mixer updates to config/logic/err checks

--- src/extension/mixer.ts
+++ src/extension/mixer.ts
@@ -4,17 +4,25 @@ import obs from './util/obs';
 import x32 from './util/x32';
 
 const obsConfig = (nodecg().bundleConfig as Configschema).obs;
+const config = (nodecg().bundleConfig as Configschema).x32;
 
-obs.conn.on('TransitionBegin', (data) => {
-  const silentScenes = [
-    obs.findScene(obsConfig.names.scenes.commercials),
-    obs.findScene(obsConfig.names.scenes.intermission),
-    obs.findScene(obsConfig.names.scenes.videoPlayer),
-  ];
-  if (silentScenes.includes(data['to-scene']) && !silentScenes.includes(data['from-scene'])) {
-    x32.fade('/dca/1/fader', 0.75, 0, 1000);
-  } else if (!silentScenes.includes(data['to-scene'])
-    && silentScenes.includes(data['from-scene'])) {
-    x32.fade('/dca/1/fader', 0, 0.75, 1000);
-  }
-});
+if (config.enable) {
+  obs.conn.on('TransitionBegin', (data) => {
+    const silentScenes = [
+      obs.findScene(obsConfig.names.scenes.commercials),
+      obs.findScene(obsConfig.names.scenes.intermission),
+      obs.findScene(obsConfig.names.scenes.videoPlayer),
+    ];
+    try {
+      if (silentScenes.includes(data['to-scene']) && !silentScenes.includes(data['from-scene'])) {
+        x32.fade('/dca/1/fader', 0.75, 0, 1000);
+      } else if (!silentScenes.includes(data['to-scene'])
+        && silentScenes.includes(data['from-scene'])) {
+        x32.fade('/dca/1/fader', 0, 0.75, 1000);
+      }
+    } catch (err) {
+      nodecg().log.warn('[Mixer] Could not change mixer fader');
+      nodecg().log.debug('[Mixer] Could not change mixer fader:', err);
+    }
+  });
+}
--- src/extension/util/x32.ts
+++ src/extension/util/x32.ts
@@ -2,6 +2,5 @@ import type { Configschema } from 'configschema';
 import X32 from 'esa-layouts-shared/x32/extension';
 import { get as nodecg } from './nodecg';
 
-const config = (nodecg().bundleConfig as Configschema).x32;
-const x32 = new X32(nodecg(), config.ip, ['/dca/1/fader']);
+const x32 = new X32(nodecg(), (nodecg().bundleConfig as Configschema).x32, ['/dca/1/fader']);
 export default x32;

e77c71afddff7992b5b555f739b3e0fe87bbbb89
zoton2 <zoton2@gmail.com>
2020-07-26 20:44:07 +0100

    Tweaks for changes to x32 stuff in esa-layouts-shared

--- src/extension/util/x32.ts
+++ src/extension/util/x32.ts
@@ -2,5 +2,5 @@ import type { Configschema } from 'configschema';
 import X32 from 'esa-layouts-shared/x32/extension';
 import { get as nodecg } from './nodecg';
 
-const x32 = new X32(nodecg(), (nodecg().bundleConfig as Configschema).x32, ['/dca/1/fader']);
+const x32 = new X32(nodecg(), (nodecg().bundleConfig as Configschema).x32);
 export default x32;

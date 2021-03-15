92fff200bfcc02478024fc350bba721d1978689c
zoton2 <zoton2@gmail.com>
2021-03-15 18:54:00 +0000

    Use component for countdown graphic from esa-layouts-shared

--- src/graphics/countdown/main.vue
+++ src/graphics/countdown/main.vue
@@ -10,53 +10,19 @@
       }"
     >
       <img class="Logo">
-      <div
-        :style="{
-          'font-size': '70px',
-          'margin-top': '50px',
-        }"
-      >
-        <span v-if="countdown.remaining > 0">
-          Event Starts In
-        </span>
-        <span v-else>
-          Event Starts Soon
-        </span>
-      </div>
-      <div
-        :style="{
-          'font-size': '200px',
-          'margin-top': '-0.2em',
-          color: 'white',
-          'font-weight': '600',
-          opacity: countdown.remaining > 0 ? 1 : 0,
-        }"
-      >
-        {{ currentCountdown }}
-      </div>
+      <countdown :style="{ 'margin-top': '50px' }" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
-import { State } from 'vuex-class';
-import { Countdown } from '@/types/schemas';
-import { msToTimeStr } from '../_misc/helpers';
+import Countdown from '@esamarathon/esa-layouts-shared/countdown/graphics';
 
-@Component
-export default class extends Vue {
-  @State countdown!: Countdown;
-
-  get currentCountdown(): string {
-    const seconds = Math.round(this.countdown.remaining / 1000);
-    if (seconds >= 60 * 60 * 10) {
-      return msToTimeStr(seconds * 1000);
-    }
-    if (seconds >= (60 * 60)) {
-      return msToTimeStr(seconds * 1000).slice(1);
-    }
-    return msToTimeStr(seconds * 1000).slice(3);
-  }
-}
+@Component({
+  components: {
+    Countdown,
+  },
+})
+export default class extends Vue {}
 </script>

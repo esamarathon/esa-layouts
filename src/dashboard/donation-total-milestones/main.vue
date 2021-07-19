3636494a53c93c95dcc887fe5727d1ebdc819409
zoton2 <zoton2@gmail.com>
2021-07-19 13:13:48 +0100

    donation-total-milestones: add sort options

--- src/dashboard/donation-total-milestones/main.vue
+++ src/dashboard/donation-total-milestones/main.vue
@@ -1,15 +1,25 @@
 <template>
   <v-app :style="{ 'min-height': '250px' }">
-    <div class="mb-2">
-      <span class="font-weight-bold">Current Donation Total:</span>
-      ${{ formatAmount(total) }}
+    <div class="mb-2 d-flex">
+      <div>
+        <span class="font-weight-bold">Donation Total:</span>
+        ${{ formatAmount(total) }}
+      </div>
+      <v-spacer />
+      <div>
+        <v-radio-group v-model="sortOpt" row class="pa-0 ma-0" hide-details label="Sort By">
+          <v-radio label="Added" :value="0" />
+          <v-radio label="Name" :value="1" />
+          <v-radio label="Amount" :value="2" class="mr-0" />
+        </v-radio-group>
+      </div>
     </div>
     <v-btn @click="addBlank">Add New Milestone</v-btn>
-    <div v-if="!milestones.length" class="pa-3 font-italic">
+    <div v-if="!milestonesSorted.length" class="pa-3 font-italic">
       No milestones created, add a new one with the button above.
     </div>
     <template v-else>
-      <milestone v-for="milestone in milestones" :key="milestone.id" :milestone="milestone" />
+      <milestone v-for="milestone in milestonesSorted" :key="milestone.id" :milestone="milestone" />
     </template>
   </v-app>
 </template>
@@ -18,6 +28,7 @@
 import { DonationTotal, DonationTotalMilestones } from '@esa-layouts/types/schemas';
 import { Vue, Component } from 'vue-property-decorator';
 import { replicantNS } from '@esa-layouts/browser_shared/replicant_store';
+import { sortBy } from 'lodash';
 import { storeModule } from './store';
 import Milestone from './components/Milestone.vue';
 
@@ -31,6 +42,18 @@ export default class extends Vue {
   @replicantNS.State(
     (s) => s.reps.donationTotalMilestones,
   ) readonly milestones!: DonationTotalMilestones;
+  sortByName = false;
+  sortOpt = 2;
+
+  get milestonesSorted(): DonationTotalMilestones {
+    if (this.sortOpt === 1) {
+      return sortBy(this.milestones, ['name']);
+    }
+    if (this.sortOpt === 2) {
+      return sortBy(this.milestones, (o) => (o.addition ? this.total + o.addition : o.amount));
+    }
+    return this.milestones;
+  }
 
   formatAmount(val: number): string {
     return val.toLocaleString('en-US', { maximumFractionDigits: 0 });

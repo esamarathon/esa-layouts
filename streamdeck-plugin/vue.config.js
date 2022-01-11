ca5173a0ab076e8be7438d6432481ec1d1a82456
zoton2 <zoton2@gmail.com>
2022-01-11 01:20:34 +0000

    Change over to Vue streamdeck-plugin, no content yet

---
+++ streamdeck-plugin/vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  lintOnSave: false,
+  outputDir: 'com.esamarathon.streamdeck.sdPlugin',
+  pages: {
+    backend: 'src/backend/main.ts',
+    pi: 'src/pi/main.ts',
+    settings: 'src/settings/main.ts',
+  },
+  publicPath: './',
+};

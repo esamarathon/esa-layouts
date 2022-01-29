42d47255f3b027399aa0c5ef84e4b1a03cf629b6
zoton2 <zoton2@gmail.com>
2022-01-29 13:29:08 +0000

    graphics\omnibar: make sure ticker msgs keep line breaks, change styling

--- src/graphics/omnibar/components/Ticker/GenericMsg.vue
+++ src/graphics/omnibar/components/Ticker/GenericMsg.vue
@@ -2,12 +2,13 @@
   <div
     class="Flex"
     :style="{
-      'font-size': '33px',
+      'font-size': lines < 2 ? '33px' : '29px',
       'white-space': 'nowrap',
-      'font-weight': 500,
+      'text-align': 'center',
+      'line-height': '100%',
     }"
   >
-    {{ msg }}
+    <span :style="{ 'white-space': 'pre' }">{{ msg }}</span>
   </div>
 </template>
 
@@ -21,6 +22,10 @@ import { Vue, Component, Prop } from 'vue-property-decorator';
 export default class extends Vue {
   @Prop({ type: String, default: 'Message?' }) readonly msg!: string;
   @Prop({ type: Number, default: 25 }) readonly seconds!: number;
+
+  get lines(): number {
+    return this.msg.split('\n').length;
+  }
 
   async created(): Promise<void> {
     await wait(this.seconds * 1000); // Wait the specified length.

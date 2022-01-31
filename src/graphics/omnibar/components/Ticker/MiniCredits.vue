6c44e58c5e769d6cb8e8920cd2cd561acf9038c1
zoton2 <zoton2@gmail.com>
2022-01-31 22:04:23 +0000

    graphics\omnibar: improve MiniCredits disposal

--- src/graphics/omnibar/components/Ticker/MiniCredits.vue
+++ src/graphics/omnibar/components/Ticker/MiniCredits.vue
@@ -29,7 +29,6 @@
 </template>
 
 <script lang="ts">
-import { wait } from '@esa-layouts/graphics/_misc/helpers';
 import { Vue, Component, Prop, Ref } from 'vue-property-decorator';
 import gsap from 'gsap';
 import { Configschema } from '@esa-layouts/types/schemas';
@@ -43,29 +42,41 @@ export default class extends Vue {
   @Ref('MiniCredits') elem!: HTMLDivElement;
   @Ref('Msg') msgElem!: HTMLDivElement;
   config = nodecg.bundleConfig as Configschema;
+  timeline: gsap.core.Timeline | undefined;
+  timeout = 0;
 
   async created(): Promise<void> {
-    // TODO: Allow the animation to be cancelled (not sure if needed but good practice to).
-    await wait(4000);
-    const { paddingLeft, paddingRight } = getComputedStyle(this.elem);
-    const maxWidth = this.elem.clientWidth - parseFloat(paddingLeft) - parseFloat(paddingRight);
-    const msgWidth = this.msgElem.clientWidth;
-    const scrollAmount = msgWidth - maxWidth;
-    if (scrollAmount > 0) {
-      gsap.to(this.msgElem, {
-        x: `-${scrollAmount}px`,
-        duration: this.seconds - 4,
-        ease: 'none',
-        onComplete: () => {
-          window.setTimeout(() => {
-            this.$emit('end');
-          }, 2000);
-        },
-      });
-    } else {
-      await wait((this.seconds - 4) * 1000); // Wait the specified length.
-      this.$emit('end');
-    }
+    this.timeout = window.setTimeout(() => {
+      const { paddingLeft, paddingRight } = getComputedStyle(this.elem);
+      const maxWidth = this.elem.clientWidth - parseFloat(paddingLeft) - parseFloat(paddingRight);
+      const msgWidth = this.msgElem.clientWidth;
+      const scrollAmount = msgWidth - maxWidth;
+      if (scrollAmount > 0) {
+        this.timeline = gsap.timeline({
+          onComplete: () => {
+            window.setTimeout(() => {
+              this.$emit('end');
+            }, 2000);
+          },
+        });
+        this.timeline.to(this.msgElem, {
+          x: `-${scrollAmount}px`,
+          duration: this.seconds - 4,
+          ease: 'none',
+        });
+      } else {
+        window.clearTimeout(this.timeout);
+        this.timeout = window.setTimeout(() => {
+          this.$emit('end');
+        }, 4000); // Wait the specified length.
+      }
+    }, 4000); // Wait the specified length.
+  }
+
+  beforeDestroy(): void {
+    this.timeline?.kill();
+    delete this.timeline;
+    window.clearTimeout(this.timeout);
   }
 }
 </script>

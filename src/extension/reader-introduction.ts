a72074e0110a76163d1e539fa0180bb1c4b2c221
zoton2 <zoton2@gmail.com>
2022-02-03 01:35:30 +0000

    Reader Introduction: add Stream Deck buttons for advancing/resetting slides

--- src/extension/reader-introduction.ts
+++ src/extension/reader-introduction.ts
@@ -7,8 +7,13 @@ import { join } from 'path';
 import { get as nodecg } from './util/nodecg';
 import { assetsReaderIntroductionImages, readerIntroduction } from './util/replicants';
 import { sc } from './util/speedcontrol';
+import sd from './util/streamdeck';
 
 let assetsTemp: Asset[] = [];
+const sdButtonUUIDMap = {
+  advanceSlide: 'com.esamarathon.streamdeck.readerintroduction-advanceslide',
+  resetSlides: 'com.esamarathon.streamdeck.readerintroduction-resetslides',
+};
 
 /**
  * Returns images sorted by name ascending.
@@ -74,19 +79,79 @@ async function downloadBoxart(id: string): Promise<void> {
   }
 }
 
+/**
+ * Correctly changes the title text on the Stream Deck "Advance Slide" buttons.
+ * @param i The index you wish to force this to display, 0-based.
+ */
+function changeAdvanceSlideSDTitle(i?: number): void {
+  let index = typeof i === 'number'
+    ? i
+    : assetsTemp.findIndex((a) => a.sum === readerIntroduction.value.current);
+  if (readerIntroduction.value.current === 'RunInfo') {
+    index = assetsTemp.length;
+  }
+  const prependText = index < assetsTemp.length
+    ? 'Advance\nSlide'
+    : 'Slides\nComplete!';
+  sd.setTextOnAllButtonsWithAction(
+    sdButtonUUIDMap.advanceSlide,
+    `${prependText}\n(${index >= 0 ? (index + 1) : '?'}/${assetsTemp.length + 1})`,
+  );
+}
+
+/**
+ * Resets the temporarily stored assets ands the current slide value.
+ */
+function reset(): void {
+  assetsTemp = assetsSorted();
+  readerIntroduction.value.current = assetsTemp[0].sum || 'RunInfo';
+  changeAdvanceSlideSDTitle();
+}
+
+/**
+ * Shows the next slide, either one of the images or if all done, the run information.
+ */
+function showNext(): void {
+  if (readerIntroduction.value.current !== 'RunInfo') {
+    const index = assetsTemp.findIndex((a) => a.sum === readerIntroduction.value.current);
+    if (index >= 0 && assetsTemp.length > (index + 1)) {
+      readerIntroduction.value.current = assetsTemp[index + 1].sum;
+    } else {
+      readerIntroduction.value.current = 'RunInfo';
+    }
+    changeAdvanceSlideSDTitle(index + 1);
+  }
+}
+
 // Listens for current/next run ID changes and executes a boxart lookup/download.
 // Also resets the temporary assets array and current ID that should be shown on the graphic.
 // TODO: Should also do this if the game name in the run data is changed?
-//       Also maybe one day integrate this into nodecg-speedcontrol?
+//       Also maybe one day integrate the download into nodecg-speedcontrol?
 let init = false;
 sc.runDataActiveRunSurrounding.on('change', async (newVal, oldVal) => {
   if (!init && newVal.current) await downloadBoxart(newVal.current);
   if (init) {
+    if (newVal.current !== oldVal?.current) reset();
     if (newVal.next && newVal.next !== oldVal?.next) await downloadBoxart(newVal.next);
-    if (newVal.current !== oldVal?.current) {
-      assetsTemp = assetsSorted();
-      readerIntroduction.value.current = assetsTemp[0].sum || 'RunInfo';
-    }
   }
   init = true;
+});
+assetsReaderIntroductionImages.on('change', (newVal) => {
+  if (!assetsTemp.length && newVal.length > 0) reset();
+});
+
+// What to do once Stream Deck connection is initialised.
+sd.on('init', () => {
+  changeAdvanceSlideSDTitle();
+});
+
+// What to do when any key is lifted on a connected Stream Deck.
+sd.on('keyUp', (data) => {
+  if (data.action === sdButtonUUIDMap.advanceSlide) {
+    showNext();
+    sd.send({ event: 'showOk', context: data.context });
+  } else if (data.action === sdButtonUUIDMap.resetSlides) {
+    reset();
+    sd.send({ event: 'showOk', context: data.context });
+  }
 });

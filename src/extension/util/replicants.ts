007677a4aa5fbae141c87726ac7aa050c17ab6aa
zoton2 <zoton2@gmail.com>
2022-02-13 11:40:07 +0100

    Add Twitch title/category changing functionality, also adds current donation total

--- src/extension/util/replicants.ts
+++ src/extension/util/replicants.ts
@@ -2,6 +2,7 @@
 
 import { Bids, BigbuttonPlayerMap, CapturePositions, Commentators, Countdown, CurrentRunDelay, DelayedTimer, DonationReader, DonationsToRead, DonationTotal, DonationTotalMilestones, GameLayouts, MusicData, NameCycle, NotableDonations, ObsData, Omnibar, OtherStreamData, Prizes, ReaderIntroduction, ServerTimestamp, StreamDeckData, TtsVoices, UpcomingRunID, VideoPlayer } from '@esa-layouts/types/schemas';
 import type { Asset } from '@shared/types';
+import { TwitchAPIData, TwitchChannelInfo } from 'speedcontrol-util/types/speedcontrol/schemas';
 import { get as nodecg } from './nodecg';
 
 /**
@@ -35,5 +36,7 @@ export const readerIntroduction = nodecg().Replicant<ReaderIntroduction>('reader
 export const serverTimestamp = nodecg().Replicant<ServerTimestamp>('serverTimestamp');
 export const streamDeckData = nodecg().Replicant<StreamDeckData>('streamDeckData');
 export const ttsVoices = nodecg().Replicant<TtsVoices>('ttsVoices');
+export const twitchAPIData = nodecg().Replicant<TwitchAPIData>('twitchAPIData', 'nodecg-speedcontrol');
+export const twitchChannelInfo = nodecg().Replicant<TwitchChannelInfo>('twitchChannelInfo', 'nodecg-speedcontrol');
 export const upcomingRunID = nodecg().Replicant<UpcomingRunID>('upcomingRunID');
 export const videoPlayer = nodecg().Replicant<VideoPlayer>('videoPlayer');

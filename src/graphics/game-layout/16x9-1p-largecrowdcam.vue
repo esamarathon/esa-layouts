5316bda997ff3b5cfdcdb257161a882d44c7bf0d
zoton2 <zoton2@gmail.com>
2022-02-16 18:39:16 +0100

    graphics\game-layout: add 16x9-1p-largecrowdcam

---
+++ src/graphics/game-layout/16x9-1p-largecrowdcam.vue
@@ -0,0 +1,124 @@
+<template>
+  <div>
+    <!-- Game Captures -->
+    <game-capture
+      id="GameCapture1"
+      class="BorderLeft BorderBottom"
+      :style="{
+        left: '533px',
+        top: '0px',
+        width: '1387px',
+        height: '780px',
+      }"
+    />
+
+    <!-- Crowd Camera Capture -->
+    <div
+      id="CameraCaptureCrowd"
+      class="Capture"
+      :style="{
+        left: '0px',
+        top: '0px',
+        width: '533px',
+        height: '325px',
+      }"
+    />
+
+    <!-- Camera Captures -->
+    <div
+      id="CameraCapture1"
+      class="Capture BorderTop"
+      :style="{
+        left: '0px',
+        top: '325px',
+        width: '533px',
+        height: '325px',
+      }"
+    />
+
+    <!-- Run Game Info/Timer -->
+    <div
+      class="Fixed Flex"
+      :style="{
+        left: '533px',
+        top: '780px',
+        width: '1387px',
+        height: '160px',
+      }"
+    >
+      <run-info
+        class="BorderLeft"
+        :style="{
+          'font-size': '45px',
+          'width': '1000px',
+          height: '100%',
+        }"
+      />
+      <timer
+        class="BorderLeft"
+        :style="{
+          'width': '387px',
+          height: '100%',
+        }"
+      />
+    </div>
+
+    <!-- Player/Commetator -->
+    <div
+      class="Fixed"
+      :style="{
+        left: '0px',
+        top: '650px',
+        width: '533px',
+      }"
+    >
+      <player />
+      <comm-and-reader />
+    </div>
+
+    <!-- Media Box -->
+    <media-box
+      :font-size="30"
+      :style="{
+        left: '0px',
+        top: '740px',
+        width: '533px',
+        height: '200px',
+      }"
+    />
+
+    <!-- Donation Bar -->
+    <donation-bar
+      :style="{
+        left: '0px',
+        top: '940px',
+        width: '1920px',
+        height: '60px',
+      }"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import MediaBox from '@shared/graphics/mediabox';
+import GameCapture from './components/GameCapture.vue';
+import Player from './components/Player.vue';
+import CommAndReader from './components/CommAndReader.vue';
+import RunInfo from './components/RunInfo.vue';
+import Timer from './components/Timer.vue';
+import DonationBar from './components/DonationBar.vue';
+
+@Component({
+  components: {
+    GameCapture,
+    Player,
+    CommAndReader,
+    RunInfo,
+    Timer,
+    MediaBox,
+    DonationBar,
+  },
+})
+export default class extends Vue {}
+</script>

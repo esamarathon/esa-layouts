61031814c549ee9887dd3132ce7fdb8731bbaf7b
zoton2 <zoton2@gmail.com>
2024-02-03 15:50:53 +0000

    Add Twitch commercial stuff for Companion

--- src/extension/companion.ts
+++ src/extension/companion.ts
@@ -1,18 +1,28 @@
 import companion from './util/companion';
+import { get as nodecg } from './util/nodecg';
 import { streamDeckData } from './util/replicants';
 import { sc } from './util/speedcontrol';
+
+// Replicants only applicable to this file from another bundle.
+const twitchCommercialsDisabled = nodecg().Replicant<boolean>('disabled', 'esa-commercials');
 
 // Sending replicant data on any changes.
 sc.timer.on('change', (value) => companion.send({ name: 'timer', value }));
 sc.timerChangesDisabled.on('change', (value) => (
   companion.send({ name: 'timerChangesDisabled', value })));
 streamDeckData.on('change', (value) => companion.send({ name: 'streamDeckData', value }));
+sc.twitchCommercialTimer.on('change', (value) => (
+  companion.send({ name: 'twitchCommercialTimer', value })));
+twitchCommercialsDisabled.on('change', (value) => (
+  companion.send({ name: 'twitchCommercialsDisabled', value })));
 
 // Sending things on connection.
 companion.evt.on('open', (socket) => {
   companion.send({ name: 'timer', value: sc.timer.value }, socket);
   companion.send({ name: 'timerChangesDisabled', value: sc.timerChangesDisabled.value }, socket);
   companion.send({ name: 'streamDeckData', value: streamDeckData.value });
+  companion.send({ name: 'twitchCommercialTimer', value: sc.twitchCommercialTimer.value });
+  companion.send({ name: 'twitchCommercialsDisabled', value: twitchCommercialsDisabled.value });
 });
 
 // Listening for any actions triggered from Companion.
@@ -49,6 +59,14 @@ companion.evt.on('action', async (name, value) => {
       delete streamDeckData.value.playerHUDTriggerType;
     } else {
       streamDeckData.value.playerHUDTriggerType = val;
+    }
+  // Used to disable the Twitch commercials for the remainder of a run.
+  } else if (name === 'twitch_commercials_disable') {
+    if (!twitchCommercialsDisabled.value
+    && !['stopped', 'finished'].includes(sc.timer.value.state)) {
+      // Sends a message to the esa-commercials bundle.
+      // Because we are using server-to-server messages, no confirmation yet.
+      nodecg().sendMessageToBundle('disable', 'esa-commercials');
     }
   }
 });

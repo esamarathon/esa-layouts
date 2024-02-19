4a01a5f370106d383216c92ee6f526c8c253b6bb
zoton2 <zoton2@gmail.com>
2024-02-19 15:52:51 +0100

    Add version of new 1p layout for GBA

---
+++ src/graphics/game-layout/esaw24-gba-1p.vue
@@ -0,0 +1,21 @@
+<template>
+  <layout-base
+    game-left="750px"
+    game-width="1170px"
+    camera-width="750px"
+  />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import LayoutBase from './esaw24-base.vue';
+
+@Component({
+  components: {
+    LayoutBase,
+  },
+})
+export default class extends Vue {
+  // code
+}
+</script>
--- src/graphics/game-layout/list.ts
+++ src/graphics/game-layout/list.ts
@@ -25,8 +25,9 @@ import L_5x2_1p from './5x2-1p.vue';
 import L_9x16_1p_2Cams from './9x16-1p-2cams.vue';
 import L_9x16_1p from './9x16-1p.vue';
 import L_DS_1p from './ds-1p.vue';
-import L_ESAW24_4x3_1p from './esaw24-4x3-1p.vue';
 import L_ESAW24_16x9_1p from './esaw24-16x9-1p.vue';
+import L_ESAW24_4x3_1p from './esaw24-4x3-1p.vue';
+import L_ESAW24_GBA_1p from './esaw24-gba-1p.vue';
 import L_FullCam from './full-cam.vue';
 import L_GB_1p from './gb-1p.vue';
 import L_GB_2p_ExtraSpace from './gb-2p-extraspace.vue';
@@ -39,5 +40,5 @@ import L_SWCF_16x9_2p_Bingo from './swcf_16x9-2p-bingo.vue';
 import L_SWCF_16x9_4p_Bingo from './swcf_16x9-4p-bingo.vue';
 import L_Taskmaster_Timer from './taskmaster-timer.vue';
 
-export { L_16x9_1p, L_16x9_1p_2Cams, L_16x9_1p_2Cams_NoGame, L_16x9_1p_HEK, L_16x9_1p_LargeCam, L_16x9_1p_LargeCrowdCam, L_16x9_2p, L_16x9_2p_Bingo, L_16x9_2p_HEK, L_16x9_3p, L_2CamsOnly, L_3DS_1p, L_4x3_1p, L_4x3_1p_2Cams, L_4x3_2p, L_4x3_2p_ExtraMiddleSpace, L_4x3_2p_ExtraSpace, L_4x3_3p, L_4x3_4p, L_5x2_1p, L_9x16_1p, L_9x16_1p_2Cams, L_DS_1p, L_ESAW24_4x3_1p, L_ESAW24_16x9_1p, L_FullCam, L_GBA_1p, L_GBA_2p, L_GB_1p, L_GB_2p_ExtraSpace, L_PokemonEmerald_MapRando, L_SM64_PSP_2p, L_SWCF_16x9_2Feeds_DancePad as L_SWCF_16x9_2Feeds_Dancepad, L_SWCF_16x9_2p_Bingo, L_SWCF_16x9_4p_Bingo, L_Taskmaster_Timer };
+export { L_16x9_1p, L_16x9_1p_2Cams, L_16x9_1p_2Cams_NoGame, L_16x9_1p_HEK, L_16x9_1p_LargeCam, L_16x9_1p_LargeCrowdCam, L_16x9_2p, L_16x9_2p_Bingo, L_16x9_2p_HEK, L_16x9_3p, L_2CamsOnly, L_3DS_1p, L_4x3_1p, L_4x3_1p_2Cams, L_4x3_2p, L_4x3_2p_ExtraMiddleSpace, L_4x3_2p_ExtraSpace, L_4x3_3p, L_4x3_4p, L_5x2_1p, L_9x16_1p, L_9x16_1p_2Cams, L_DS_1p, L_ESAW24_16x9_1p, L_ESAW24_4x3_1p, L_ESAW24_GBA_1p, L_FullCam, L_GBA_1p, L_GBA_2p, L_GB_1p, L_GB_2p_ExtraSpace, L_PokemonEmerald_MapRando, L_SM64_PSP_2p, L_SWCF_16x9_2Feeds_DancePad as L_SWCF_16x9_2Feeds_Dancepad, L_SWCF_16x9_2p_Bingo, L_SWCF_16x9_4p_Bingo, L_Taskmaster_Timer };
 export const defaultCode = 'esaw24-4x3-1p';
